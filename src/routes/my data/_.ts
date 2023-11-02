f159d1e854648c8ef09f8629fa5656d7f0980f08
TheNolle <github@thenolle.com>
2023-11-02 16:47:25 +0100

    Added "/socials" route to "/mydata"

--- src/routes/my data/_.ts
+++ src/routes/my data/_.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 
 import Route1 from './mycurrentproject'
+import Route2 from './socials'
 
 const router = express.Router()
 
 router.use('/mycurrentproject', Route1)
+router.use('/socials', Route2)
 
 export default router
